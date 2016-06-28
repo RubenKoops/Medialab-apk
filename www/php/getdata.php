d4491d978728498e450338b1a6a43e57a96f2ea2
apielotje <info@rubenkoops.nl>
2016-06-28 17:32:35 +0200

    Nieuwe getdata.php

--- www/php/getdata.php
+++ www/php/getdata.php
@@ -15,45 +15,101 @@ $action = $_POST['action'];
 //"SELECT id FROM `inventory`"
 
 include "includes.php";
-	
-$data=array();
-$q=mysql_query("SELECT * FROM `Medialab_inventory` ORDER BY date DESC");
-while ($row=mysql_fetch_object($q)){
-	$data[]=$row;
-	
-	//maakt standaard afb als afb ontbreekt
-	/*
-	$id = $row->id;
-	if(!file_exists("../recepten/".$id."_main.jpg")){
-		echo "../recepten/".$id."_main.jpg bestaat niet <br>";
-		copy("../img/no-image.jpg","../recepten/".$id."_main.jpg");
-		}*/
-	
-}
-echo json_encode($data);
 
-foreach(glob("../recepten/*main.jpg") as $img){
-	
-		//echo $img.'<br>';
-		//echo resize($img,"_thumb",300);
+$id=strtoupper($_POST['id']);
+$type=$_POST['type'];
+$name=$_POST['name'];
+$date= date("Y-m-d H:i:s");
+$editdate= $date;
+$owner=$_POST['owner'];
+
+//select, insert, update
+$action = $_POST['action'];
+//cols en values gescheiden met komma
+$cols = $_POST['cols'];
+$values = $_POST['values'];
+//table name
+$table = $_POST['table'];
+//ordenen coll
+$keycol = $_POST['keycol'];
+$keyvalue = $_POST['keyvalue'];
+//ASC of DESC
+$order = $_POST['order'];
+
+
+if(!$_POST['action']){
+	echo "geen data";
+	}
+//////////////////////////////////////////////////////////
+//														//
+//						  SELECT						//
+//														//
+//////////////////////////////////////////////////////////
+//"SELECT * FROM `inventory` ORDER BY date DESC"
+if($action == 'select'){
+	$data=array();
+	$q=mysql_query("SELECT $cols FROM $table ORDER BY $keycol $order");
+	while ($row=mysql_fetch_object($q)){
+		$data[]=$row;
+		//$id = $row->id;
 		}
+	echo json_encode($data);
+	}
+//////////////////////////////////////////////////////////
+//														//
+//						  INSERT						//
+//														//
+//////////////////////////////////////////////////////////
+//"INSERT INTO inventory (id,type,name,date,editdate,owner) VALUES ('$id','$type','$name','$date','$date','$owner')"
+if($action == 'insert'){
+	$values_array = explode(",",$values);
+	$values_string = implode("','", $values_array);
+	$values_string = "'".$values_string."'";
 	
-
-
-function resize($img,$increment,$dst_width){
-
-$new_name = str_replace(".jpg",$increment.".jpg",$img);
-//new folder
-list($width_orig, $height_orig) = getimagesize($img);
-//ne size
-$dst_height = ($dst_width/$width_orig)*$height_orig;
-$im = imagecreatetruecolor($dst_width,$dst_height);
-$image = imagecreatefromjpeg($img);
-imagecopyresampled($im, $image, 0, 0, 0, 0, $dst_width, $dst_height, $width_orig, $height_orig);
-//modive the name as u need
-imagejpeg($im,$new_name);
-//save memory
-imagedestroy($im);
-return "image saved as: ".$new_name;
-}
+	$q=mysql_query("INSERT INTO $table ($cols) VALUES ($values_string)");
+	if($q){
+		echo "ok";
+		}else{
+			echo "Error message = ".mysql_error();
+			}
+		  
+	}
+//////////////////////////////////////////////////////////
+//														//
+//						  UPDATE						//
+//														//
+//////////////////////////////////////////////////////////
+//"UPDATE inventory SET type='$type', name='$name', editdate='$editdate', owner='$owner' WHERE id='$id'"
+if($action == 'update'){
+	$values_array = explode(",",$values);
+	$cols_array = explode(",",$cols);
+	$count = count($cols_array);
+	while($x <= $count){
+		$update_array[] = $cols_array[$x]."='".$values_array[$x]."'";
+		$x++;
+		}
+	$update_string = implode(", ", $update_array);
+	
+	$q=mysql_query("UPDATE $table SET $update_string WHERE $keycol='$keyvalue'");
+	if($q){
+		echo "ok";
+		}else{
+			echo "Error message = ".mysql_error();
+			}
+		  
+	}
+//////////////////////////////////////////////////////////
+//														//
+//						  DELETE						//
+//														//
+//////////////////////////////////////////////////////////
+//"DELETE FROM inventory WHERE id='$id'"
+if($action == 'delete'){
+	$q=mysql_query("DELETE FROM $table WHERE $keycol='$keyvalue'");
+	if($q){
+		echo "ok";
+		}else{
+			echo "Error message = ".mysql_error();
+			}
+	}
 ?>
